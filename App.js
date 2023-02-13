49c69f7a5309fe492f372a146b5b0378a4308ea3
Dev-scott <sadoscott25@gmail.com>
2023-02-13 20:43:50 +0100

    mise a jour

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { Text } from "react-native";
+import { Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () =>
+    {
+        return (
+            <>
+                <StatusBar style="auto" />
+                <View style={styles.topbar}>
+                    <TouchableOpacity style={styles.topbarbutton}>
+                  
+                    </TouchableOpacity>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                    <TouchableOpacity style={styles.topbarbutton}>
+                  
+                    </TouchableOpacity>
+                </View>
+             
+            </>
+        );
+    }
+
+
+    const styles = StyleSheet.create({
+      
+        topbar: {
+            
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: '#f2f2f2',
+            paddingTop: 40,
+            paddingBottom: 40,
+            // border: '1px solid #f2f2f2',
+            borderColor: '#f2f2f2',
+            // borderRadius: 10,
+            borderWidth: 1,
+
+
+
+        },
+
+        topbarbutton:{
+            padding:50,
+
+            width: 48,
+            height:48,
+            backgroundColor: '#000',
+                // borderRadius: 50,
+            justifyContent: 'center',
+            alignItems: 'center',
+
+        }
+
+
+    })
+
+export default App;
